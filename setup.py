e1e8f68e806f4280b4cc0e85212b82f6a5f22326
Alistair King <alistair@caida.org>
2014-12-15 07:20:51 -0800

    configure distutils to build _pybgpstream

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from distutils.core import setup, Extension
-setup(name="_pybgpstream", version="1.0",
-      ext_modules=[
-         Extension("_pybgpstream", ["src/_pybgpstream.c"]),
-         ])
+
+_pybgpstream_module = Extension("_pybgpstream",
+                                libraries = ["bgpstream"],
+                                sources = ["src/_pybgpstream.c"])
+
+setup(name = "_pybgpstream",
+      description = "TODO",
+      version = "1.0",
+      author = "Alistair King",
+      author_email = "bgpstream-info@caida.org",
+      ext_modules = [_pybgpstream_module,])
