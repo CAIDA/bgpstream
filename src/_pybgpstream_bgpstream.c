bae832cd412babe53b0570aaa37c52bb5094669a
Alistair King <alistair@caida.org>
2015-03-05 14:00:09 -0800

    expose data interface selection

--- src/_pybgpstream_bgpstream.c
+++ src/_pybgpstream_bgpstream.c
@@ -134,48 +134,77 @@ BGPStream_add_interval_filter(BGPStreamObject *self, PyObject *args)
   Py_RETURN_NONE;
 }
 
-#if 0
+/** Get information about available data interfaces */
+static PyObject *
+BGPStream_get_data_interfaces(BGPStreamObject *self)
+{
+  PyObject *list;
+  PyObject *dict;
+
+  int id_cnt = 0;
+  bgpstream_data_interface_id_t *ids = NULL;
+  int i;
+  bgpstream_data_interface_info_t *info = NULL;
+
+  /* create the list */
+  if((list = PyList_New(id_cnt)) == NULL)
+    return NULL;
+
+  id_cnt = bgpstream_get_data_interfaces(self->bs, &ids);
+  for(i=0; i<id_cnt; i++) {
+
+    /* create the dictionary */
+    if((dict = PyDict_New()) == NULL)
+      return NULL;
+
+    info = bgpstream_get_data_interface_info(self->bs, ids[i]);
+
+    /* add info to dict */
+
+    /* id */
+    if(PyDict_SetItem(dict, PyString_FromString("id"),
+                      PyInt_FromLong(ids[i])) == -1)
+      return NULL;
+
+    /* name */
+    if(PyDict_SetItem(dict, PyString_FromString("name"),
+                      PyString_FromString(info->name)) == -1)
+      return NULL;
+
+    /* description */
+    if(PyDict_SetItem(dict, PyString_FromString("description"),
+                      PyString_FromString(info->description)) == -1)
+      return NULL;
+
+    /* add dict to list */
+    if(PyList_Append(list, dict) == -1)
+      return NULL;
+  }
+
+  return list;
+}
+
 /** Set the data interface */
 static PyObject *
 BGPStream_set_data_interface(BGPStreamObject *self, PyObject *args)
 {
-  /* args: datasource (string) */
-  static char *datasource_strs[] = {
-    "mysql",
-    "custom-list",
-    "csvfile",
-    NULL,
-  };
-  static int datasource_vals[] = {
-    BS_MYSQL,
-    BS_CUSTOMLIST,
-    BS_CSVFILE,
-    -1,
-  };
-
-  const char *datasource;
-  if (!PyArg_ParseTuple(args, "s", &datasource)) {
+  int id;
+  char *name;
+  if (!PyArg_ParseTuple(args, "s", &name)) {
     return NULL;
   }
 
-  int i;
-  int datasource_val = -1;
-  for (i=0; datasource_strs[i] != NULL; i++) {
-    if (strcmp(datasource_strs[i], datasource) == 0) {
-      datasource_val = datasource_vals[i];
-      break;
-    }
-  }
-  if (datasource_val == -1) {
+  if((id = bgpstream_get_data_interface_id_by_name(self->bs, name)) == 0) {
     return PyErr_Format(PyExc_ValueError,
-			"Invalid datasource: %s", datasource);
+			"Invalid data interface: %s", name);
   }
 
-  bgpstream_set_data_interface(self->bs, datasource_val);
+  bgpstream_set_data_interface(self->bs, id);
 
   Py_RETURN_NONE;
 }
 
+#if 0
 /** Set a data interface option */
 static PyObject *
 BGPStream_set_data_interface_option(BGPStreamObject *self, PyObject *args)
@@ -292,14 +321,19 @@ static PyMethodDef BGPStream_methods[] = {
     METH_VARARGS,
    "Add an interval filter to an un-started stream."
   },
-#if 0
+  {
+    "get_data_interfaces",
+    (PyCFunction)BGPStream_get_data_interfaces,
+    METH_NOARGS,
+    "Get a list of data interfaces available",
+  },
   {
     "set_data_interface",
     (PyCFunction)BGPStream_set_data_interface,
     METH_VARARGS,
     "Set the data interface used to discover dump files"
   },
-
+#if 0
   {
     "set_data_interface_option",
     (PyCFunction)BGPStream_set_data_interface_option,
