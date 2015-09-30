b603c8a434eb9972fbebe3cc664542d865694470
Alistair King <alistair@caida.org>
2015-09-30 11:42:50 -0700

    Fix autogen script

--- autogen.sh
+++ autogen.sh
@@ -23,10 +23,6 @@
 # along with libbgpstream.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# The --force option rebuilds the configure script regardless of its timestamp in relation to that of the file configure.ac.
-# The option --install copies some missing files to the directory, including the text files COPYING and INSTALL.
-# We add -I config and -I m4 so that the tools find the files that we're going to place in those subdirectories instead of in the project root.
-
-autoreconf --force --install -I config -I m4
+autoreconf -vfi
 
 
