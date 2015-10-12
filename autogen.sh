6ed7741803830c8c2078636840c9f2aeef934a73
Chiara Orsini <chiara@caida.org>
2015-10-12 10:38:33 -0700

    minor fixes to licenses

--- autogen.sh
+++ autogen.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
 #
-# libbgpstream
+# This file is part of bgpstream
 #
-# Chiara Orsini, CAIDA, UC San Diego
-# chiara@caida.org
+# CAIDA, UC San Diego
+# bgpstream-info@caida.org
 #
-# Copyright (C) 2013 The Regents of the University of California.
+# Copyright (C) 2015 The Regents of the University of California.
+# Authors: Alistair King, Chiara Orsini
 #
-# This file is part of libbgpstream.
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
 #
-# libbgpstream is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
 #
-# libbgpstream is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with libbgpstream.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 autoreconf -vfi
