04907a0f5903b9c4ccedca255f29d1625cd68041
Alistair King <alistair@caida.org>
2015-10-14 10:12:37 -0700

    Update tutorials and remove old data files

--- examples/topology.py
+++ examples/topology.py
@@ -1,22 +1,21 @@
+#!/usr/bin/env python
 #
- # This file is part of bgpstream
- #
- # Copyright (C) 2015 The Regents of the University of California.
- # Authors: Alistair King, Chiara Orsini
- #
- # All rights reserved.
- #
- # This code has been developed by CAIDA at UC San Diego.
- # For more information, contact bgpstream-info@caida.org
- #
- # This source code is proprietary to the CAIDA group at UC San Diego and may
- # not be redistributed, published or disclosed without prior permission from
- # CAIDA.
- #
- # Report any bugs, questions or comments to bgpstream-info@caida.org
- #
-
- #!/usr/bin/env python
+# This file is part of bgpstream
+#
+# Copyright (C) 2015 The Regents of the University of California.
+# Authors: Alistair King, Chiara Orsini
+#
+# All rights reserved.
+#
+# This code has been developed by CAIDA at UC San Diego.
+# For more information, contact bgpstream-info@caida.org
+#
+# This source code is proprietary to the CAIDA group at UC San Diego and may
+# not be redistributed, published or disclosed without prior permission from
+# CAIDA.
+#
+# Report any bugs, questions or comments to bgpstream-info@caida.org
+#
 
 from _pybgpstream import BGPStream, BGPRecord, BGPElem
 
@@ -26,13 +25,11 @@ stream = BGPStream()
 # create a reusable bgprecord instance
 rec = BGPRecord()
 
-# select the datasource interface
-stream.set_data_interface('singlefile')
-
-# select the MRT file to be read by the singlefile datasource
-stream.set_data_interface_option('singlefile', 'rib-file','./ris.rrc06.ribs.1427846400.gz')
-
-# No filters needed
+# configure the stream to retrieve RIB records from the RRC06 collector at
+# 2015/05/01 00:00 UTC
+stream.add_filter('collector', 'rrc06')
+stream.add_filter('record-type', 'ribs')
+stream.add_interval_filter(1427846400, 1427846700)
 
 # start the stream
 stream.start()
@@ -57,5 +54,5 @@ while(stream.get_next_record(rec)):
          elem = rec.get_next_elem()
 
 # Output results
-print "Processed ", rib_entries, " rib entries"
-print "Found ", len(as_topology), " AS adjacencies"
+print "Processed", rib_entries, "rib entries"
+print "Found", len(as_topology), "AS adjacencies"
--- examples/tutorial_print.py
+++ examples/tutorial_print.py
@@ -1,22 +1,21 @@
+#!/usr/bin/env python
 #
- # This file is part of bgpstream
- #
- # Copyright (C) 2015 The Regents of the University of California.
- # Authors: Alistair King, Chiara Orsini
- #
- # All rights reserved.
- #
- # This code has been developed by CAIDA at UC San Diego.
- # For more information, contact bgpstream-info@caida.org
- #
- # This source code is proprietary to the CAIDA group at UC San Diego and may
- # not be redistributed, published or disclosed without prior permission from
- # CAIDA.
- #
- # Report any bugs, questions or comments to bgpstream-info@caida.org
- #
-
- #!/usr/bin/env python
+# This file is part of bgpstream
+#
+# Copyright (C) 2015 The Regents of the University of California.
+# Authors: Alistair King, Chiara Orsini
+#
+# All rights reserved.
+#
+# This code has been developed by CAIDA at UC San Diego.
+# For more information, contact bgpstream-info@caida.org
+#
+# This source code is proprietary to the CAIDA group at UC San Diego and may
+# not be redistributed, published or disclosed without prior permission from
+# CAIDA.
+#
+# Report any bugs, questions or comments to bgpstream-info@caida.org
+#
 
 from _pybgpstream import BGPStream, BGPRecord, BGPElem
 
@@ -26,13 +25,12 @@ stream = BGPStream()
 # create a reusable bgprecord instance
 rec = BGPRecord()
 
-# select the datasource interface
-stream.set_data_interface('singlefile')
+# configure the stream to retrieve Updates records from the RRC06 collector
+stream.add_filter('collector', 'rrc06')
+stream.add_filter('record-type', 'updates')
 
-# select the MRT file to be read by the singlefile datasource
-stream.set_data_interface_option('singlefile', 'upd-file','./ris.rrc06.updates.1427846400.gz')
-
-# select the time interval to process  Wed Apr  1 00:02:50 UTC 2015 -> Wed Apr  1 00:04:30
+# select the time interval to process:
+# Wed Apr 1 00:02:50 UTC 2015 -> Wed Apr 1 00:04:30
 stream.add_interval_filter(1427846570, 1427846670)
 
 # start the stream
@@ -43,6 +41,7 @@ while(stream.get_next_record(rec)):
     print rec.status, rec.project +"."+ rec.collector, rec.time
     elem = rec.get_next_elem()
     while(elem):
-        print "\t", elem.type, elem.peer_address, elem.peer_asn, elem.type, elem.fields
+        print "\t", elem.type, elem.peer_address, elem.peer_asn, \
+            elem.type, elem.fields
         elem = rec.get_next_elem()
-        
+
