09c8e4cd3ffde6c5b1f6af848180126d36f56d56
Alistair King <alistair@caida.org>
2015-10-14 18:50:24 +0000

    Fix lingering non-GPL licenses

--- tools/bgpstream_sqlite_mgmt.py
+++ tools/bgpstream_sqlite_mgmt.py
@@ -1,20 +1,25 @@
 #
- # This file is part of bgpstream
- #
- # Copyright (C) 2015 The Regents of the University of California.
- # Authors: Alistair King, Chiara Orsini
- #
- # All rights reserved.
- #
- # This code has been developed by CAIDA at UC San Diego.
- # For more information, contact bgpstream-info@caida.org
- #
- # This source code is proprietary to the CAIDA group at UC San Diego and may
- # not be redistributed, published or disclosed without prior permission from
- # CAIDA.
- #
- # Report any bugs, questions or comments to bgpstream-info@caida.org
- #
+# This file is part of bgpstream
+#
+# CAIDA, UC San Diego
+# bgpstream-info@caida.org
+#
+# Copyright (C) 2015 The Regents of the University of California.
+# Authors: Alistair King, Chiara Orsini
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 
  #!/usr/bin/env python
